8987aa8120aa375d451f8395ce9b128ce4bb9562
Hüseyin ESKAN <huseyineskan01@gmail.com>
2022-12-03 00:48:40 +0100

    mobile menu done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,13 @@
-// JS CODE
+// VARIABLES
 const openMenuBtn = document.querySelector('.open-menu');
 const closeMenuBtn = document.querySelector('.close-menu');
 const nav = document.querySelector('#nav');
 
+// EVENTS
 openMenuBtn.addEventListener('click', openMenu);
 closeMenuBtn.addEventListener('click', closeMenu);
 
+// FUNCTIONS
 function openMenu(){
     nav.classList.add('menu-active');
 }
