93572e0ae29b1bbed37c4f3eac1e014ed2962fa3
Hüseyin ESKAN <huseyineskan01@gmail.com>
2022-12-03 00:40:11 +0100

    Mobile menü

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,15 @@
 // JS CODE
+const openMenuBtn = document.querySelector('.open-menu');
+const closeMenuBtn = document.querySelector('.close-menu');
+const nav = document.querySelector('#nav');
+
+openMenuBtn.addEventListener('click', openMenu);
+closeMenuBtn.addEventListener('click', closeMenu);
+
+function openMenu(){
+    nav.classList.add('menu-active');
+}
+
+function closeMenu(){
+    nav.classList.remove('menu-active');
+}
